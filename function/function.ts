33d7380498cab353bb72350b2e854a805982931e
Isuru Shehara <isurushehara@gmail.com>
2026-05-12 23:07:34 +0530

    feat: add narrowing

--- function/function.ts
+++ function/function.ts
@@ -16,3 +16,42 @@ function printHello(message: string): void {
 let addNumber = (a: number, b: number): number => {
     return a + b;
 }
+
+// Type Narrowing
+function narrow(val: string | number) {
+    if (typeof val === 'string') {
+        val.toUpperCase();
+    } else if (typeof val === 'number') {
+        val.toFixed();
+    } else {
+        return val
+    }
+
+}
+
+
+// Narrowing with Discrimination Union
+type Circle = {
+    type: "circle";
+    radius: number;
+}
+
+type Square = {
+    type: "square";
+    width: number;
+    height: number;
+}
+
+type Shape = Circle | Square
+
+function setShape(val: Shape) {
+    if (val.type === 'circle') {
+        val.radius = 7;
+    } else if (val.type === 'square') {
+        val.width = 7;
+        val.height = 7;
+    } else {
+        return val;
+    }
+
+}
