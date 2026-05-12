2dd6ede22ae2cd49fb2684b988800da15a653dbe
Isuru Shehara <isurushehara@gmail.com>
2026-05-12 09:50:49 +0530

    feat: create variables and learn compile process

---
+++ variable/variable.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// let name = "Isuru" <= In JavaScript
+let name = "Isuru";
+name = "Shehara";
+// name = 3; <= Type 'number' is not assignable to type 'string'
+console.log(name);
+//# sourceMappingURL=variable.js.map
---
+++ variable/variable.ts
@@ -0,0 +1,7 @@
+// let name = "Isuru" <= In JavaScript
+let name:string = "Isuru";
+name = "Shehara";
+
+// name = 3; <= Type 'number' is not assignable to type 'string'
+
+console.log(name);
