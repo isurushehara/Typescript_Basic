8091cd8376265a6f6a041072d939ecec6a0fefc4
Isuru Shehara <isurushehara@gmail.com>
2026-05-12 22:40:29 +0530

    feat: add literal types sample

--- variable/variable.ts
+++ variable/variable.ts
@@ -23,3 +23,55 @@ let arrOfArr: number[][] = [    // Array of Array
 ];
 let fixedArr: [string, number] = ["Isuru", 24] // Fixed Array/ Tupple
 
+// any & unknown data type
+let anyVariable: any = 4;
+anyVariable = "a";
+anyVariable.toUpperCase();
+
+let unknownVariable: unknown = 4;
+unknownVariable = "a";
+//unknownVariable.toUpperCase();  <== 'unknownVariable' is of type 'unknown'.
+if (typeof unknownVariable === "string") {
+    unknownVariable.toUpperCase();
+}
+// or
+(unknownVariable as string).toUpperCase(); // Type accession
+unknownVariable = +"10"; // Type casting (this convert to number)
+
+//Custom Types
+type stringORnumber = string | number;
+let userInput: stringORnumber = 10;
+
+// readonly Modifier
+type Person = {
+    name: string;
+    age: number;
+    readonly isAgree: boolean;
+}
+
+let isuru: Person = {
+    name: "Isuru",
+    age: 23,
+    isAgree: true
+}
+
+isuru.age = 24;
+// isuru.isAgree = false; <== Cannot assign to 'isAgree' because it is a read-only property.
+
+// optional Modifier
+type Book = {
+    name: string;
+    year: number;
+    fontsize?: number;
+};
+
+let book_1 = {
+    name: "Book 01",
+    year: 2010,
+};
+
+// Literal Types
+let direction: "North" | "West" | "East" | "South";
+direction = "North";
+// direction = "Isuru"; <== Type '"Isuru"' is not assignable to type '"North" | "West" | "East" | "South"'.
+
